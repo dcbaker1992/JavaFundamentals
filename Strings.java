49c8e8a03db7fa5632ebc61aa9ceb25227849315
Dillon <dcbaker.1992@gmail.com>
2021-09-20 13:40:47 -0500

    String length methods as well as variables were added

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    //this determines a strings length
+    public static void main(String[] asrgs){
+        String ninja = "Coding Dojo is Awesome";
+        int length = ninja.length();
+        System.out.println("The String length is : " + length );
+    //the out put will be "The String length is : 22"
+    }
+}
