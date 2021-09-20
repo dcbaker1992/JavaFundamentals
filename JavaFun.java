3357c3d5562450787f53a4d44164994d5fef63d4
Dillon <dcbaker.1992@gmail.com>
2021-09-20 11:06:54 -0500

    This is a simple printing excersize to familiarize myself with the structer of test files and class files

---
+++ JavaFun.java
@@ -0,0 +1,14 @@
+public class JavaFun {
+    
+    public void Name(){
+        System.out.println("Hello My Name Is Dillon Baker");
+    }
+    
+    public void Age(){
+        System.out.println("I am 28 years old");
+    }
+
+    public void Location(){
+        System.out.println("I live in Carrollton, Texas");
+    }
+}
---
+++ JavaFunTest.java
@@ -0,0 +1,12 @@
+public class JavaFunTest {
+    
+    public static void main(String[] args){
+       
+    JavaFun j = new JavaFun();
+
+    j.Name();
+    j.Age();
+    j.Location();
+
+    }
+}
