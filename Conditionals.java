f963efaee8c7209693de7a1cc42f408ed4e47c3b
Dillon <dcbaker.1992@gmail.com>
2021-09-21 11:13:03 -0500

    learned about using conditionals in Java

---
+++ Conditionals.java
@@ -0,0 +1,32 @@
+public class Conditionals {
+    public static void main(String[] asrgs){
+        
+        // IF
+        boolean defenderGuarding = true;
+        if(defenderGuarding == true) {
+            System.out.println("Pass The Ball");
+        }
+
+
+        // IF-ELSE 
+        boolean defenderGuarding = true;
+        if(defenderGuarding == true) {
+            System.out.println("Pass The Ball");
+        } else {
+            System.out.println("Shoot The Ball");
+        }
+
+
+
+        // IF-ELSE-IF 
+        boolean defenderGuarding = true;
+        boolean closeToBasket = true;
+        if(defenderGuarding == true) {
+            System.out.println("Pass The Ball");
+        } else if(closeToBasket == true) {
+            System.out.println("Shoot The Ball");
+        } else {
+            System.out.println("Dribble The Ball");
+}
+    }
+}
